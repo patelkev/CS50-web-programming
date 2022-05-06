2ac11695db8eba75a0505117bf3673db97e5c2ac
patelkev <77302079+patelkev@users.noreply.github.com>
2022-05-06 17:58:01 -0400

    conditional script python

--- python/learn-python.py
+++ python/learn-python.py
@@ -1,2 +1,15 @@
+from ast import If
+
+
 name = input("Name: ")
 print(f"Hello, {name} nice to meet you!")
+
+n = int(input("Number: "))
+
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
+
