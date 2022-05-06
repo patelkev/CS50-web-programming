e822b6fe985df2376fa8c479425b9b5a17522a12
patelkev <77302079+patelkev@users.noreply.github.com>
2022-05-06 18:28:00 -0400

    sequences (common DSA)  in python

--- python/learn-python.py
+++ python/learn-python.py
@@ -1,15 +1,39 @@
 from ast import If
 
+# # input
+# name = input("Name: ")
+# print(f"Hello, {name} nice to meet you!")
 
-name = input("Name: ")
-print(f"Hello, {name} nice to meet you!")
+# # conditional 
+# n = int(input("Number: "))
 
-n = int(input("Number: "))
+# if n > 0:
+#     print("The number you entered is positive")
+# elif n < 0:
+#     print("The number you entered is negative")
+# else:
+#     print("The number you entered is zero")
 
-if n > 0:
-    print("n is positive")
-elif n < 0:
-    print("n is negative")
-else:
-    print("n is zero")
+# sequences
+# list (sequences of mutable value - changable)
+name = ["harry", "Ron", "Gara"]
+name.append("Ginny")
+name.sort()
+print (name)
 
+# tuple (sequences of imutable value - unchangble)
+coordinates = (10, 20)
+
+# set (collection of unique value)
+# empty set
+s = set()
+
+# add elements
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(1)
+print(s)
+print(f"There are {len(s)} number of element in the set.")
+
+# dict (collection of key-value pair)
