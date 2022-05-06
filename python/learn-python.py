906f4451379d6f43d82aa5b49ba9318b2808a2e0
patelkev <77302079+patelkev@users.noreply.github.com>
2022-05-06 18:34:10 -0400

    Loops in python

--- python/learn-python.py
+++ python/learn-python.py
@@ -16,24 +16,32 @@ from ast import If
 
 # sequences
 # list (sequences of mutable value - changable)
-name = ["harry", "Ron", "Gara"]
-name.append("Ginny")
-name.sort()
-print (name)
+# name = ["harry", "Ron", "Gara"]
+# name.append("Ginny")
+# name.sort()
+# print (name)
 
-# tuple (sequences of imutable value - unchangble)
-coordinates = (10, 20)
+# # tuple (sequences of imutable value - unchangble)
+# coordinates = (10, 20)
 
-# set (collection of unique value)
-# empty set
-s = set()
+# # set (collection of unique value)
+# # empty set
+# s = set()
 
-# add elements
-s.add(1)
-s.add(2)
-s.add(3)
-s.add(1)
-print(s)
-print(f"There are {len(s)} number of element in the set.")
+# # add elements
+# s.add(1)
+# s.add(2)
+# s.add(3)
+# s.add(1)
+# print(s)
+# print(f"There are {len(s)} number of element in the set.")
 
 # dict (collection of key-value pair)
+
+# Loops
+names = ["harry", "Ron", "Gara", "George"]
+for name in names:
+    print(name)
+
+for i in range(10):
+    print(i)
